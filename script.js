7077266e43e96ebcc33d6059ce740dcba1e46c89
Miguel <mmirandasanchez16@gmail.com>
2024-11-06 10:53:09 -0300

    update

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const handleOpenBtn = () =>{
+    const navbar = document.querySelector('.nav-principal');
+    navbar.style.display = 'flex';
+}
+
+const handleCloseBtn = () => {
+    const navbar = document.querySelector('.nav-principal');
+    navbar.style.display = 'none';
+}
